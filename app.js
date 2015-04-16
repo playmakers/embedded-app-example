428770cd5094affe180c98bca3e49ee98c6780b6
Tobias Bielohlawek <tobi@soundcloud.com>
2015-04-16 14:43:42 +0200

    Add express

---
+++ app.js
@@ -0,0 +1,53 @@
+
+var express = require('express'),
+  morgan = require('morgan'),
+  bodyParser = require('body-parser');
+
+// //load settings from environment config
+// nconf.argv().env().file({
+//     file: (process.env.NODE_ENV || 'dev') + '-settings.json'
+// });
+// exports.nconf = nconf;
+
+//configure express
+var app = express();
+
+//log all requests
+app.use(morgan('combined'));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//setup encrypted session cookies
+// app.use(express.cookieParser());
+// app.use(express.session({
+//     secret: "--express-session-encryption-key--"
+// }));
+
+// //statically serve from the 'public' folder
+// app.use(express.static(path.join(__dirname, 'public')));
+
+//use jade templating engine for view rendering
+// app.set('view engine', 'jade');
+
+//use the environment's port if specified
+app.set('port', process.env.PORT || 3000);
+
+// var appAuth = new shopifyAuth.AppAuth();
+
+// //configure routes
+app.get('/', function (req, res) {
+  res.send('hello, world!')
+})
+// app.get('/auth_app', appAuth.initAuth);
+// app.get('/escape_iframe', appAuth.escapeIframe);
+// app.get('/auth_code', appAuth.getCode);
+// app.get('/auth_token', appAuth.getAccessToken);
+// app.get('/render_app', routes.renderApp);
+
+app.listen(app.get('port'), function() {
+  console.log('Listening on port ' + app.get('port'));
+});
